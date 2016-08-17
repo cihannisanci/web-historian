ac00a2fe5d951c688e9c182acb3b47f9069ee36e
cihannisanci <cihannisan@gmail.com>
2016-08-17 15:55:18 -0700

    Serve homepage to get requests at root directory

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,53 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  var defaultHeaders = {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10
+  };
+  console.log(req.method);
+  console.log(req.url);
+  if (req.method === 'GET') {
+    
+    if (req.url.search('css') !== -1) {
+      
+
+      fs.readFile('public/styles.css', function(err, content) {
+        defaultHeaders['Content-Type'] = 'text/css';
+        res.writeHead('200', defaultHeaders);         
+        res.end(content);
+      });
+
+    }
+
+    fs.readFile( 'public/index.html', function(err, content) {
+      defaultHeaders['Content-Type'] = 'text/html';
+      res.writeHead('200', defaultHeaders);
+      res.end(content);
+    });
+
+
+
+  
+
+  } else if (req.method === 'POST') {
+    var message = '';
+
+    req.on('data', function(chunk) {
+      message += chunk;
+
+    });
+
+    req.on('end', function(data) {
+      res.end(message);
+    });
+
+    //res.end(JSON.stringify(req.headers));
+  }
 };
